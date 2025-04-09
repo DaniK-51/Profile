92b07983a35373f7a5c358c32bfdf81fa348a073
DaniK_51 <danijarfairushin@gmail.com>
2025-04-09 18:17:19 +0500

    Program released

--- stat_tube_pos.py
+++ stat_tube_pos.py
@@ -1,8 +1,15 @@
 from cs_change import *
-import math
-import turtle
-from profile_generator import *
-import csv
+
+
+def bin_finder(arr: list[float], target: float):
+    l, r = 0, len(arr) - 1
+    while l != r:
+        c = l + (r - l) // 2 + 1
+        if arr[c] > target:
+            r = c - 1
+        else:
+            l = c
+    return l
 
 
 def stat_tube_pos(profile: list[list[float]], dx: float, r: float, angle: float = 0):
@@ -44,24 +51,55 @@ def stat_tube_pos(profile: list[list[float]], dx: float, r: float, angle: float 
             l3 = (n3[0] ** 2 + n3[1] ** 2) ** .5
             n3 = [n3[0] / l3 * (2 / (1 + (n1[0] * n2[0] + n1[1] * n2[1]))) ** 0.5,
                   n3[1] / l3 * (2 / (1 + (n1[0] * n2[0] + n1[1] * n2[1]))) ** 0.5]
-            print(n3)
             ext_profile.append([profile[i][0] + n3[0] * r, profile[i][1] + n3[1] * r])
 
-    t = turtle.Turtle()
-    s = turtle.Screen().getcanvas()
-    k = 0.5
-    for i in range(len(ext_profile)):
-        s.create_line(ext_profile[i-1][0] * k, -ext_profile[i-1][1] * k, ext_profile[i][0] * k, -ext_profile[i][1] * k, fill="blue")
-    for i in range(n):
-        s.create_line(profile[i-1][0] * k, -profile[i-1][1] * k, profile[i][0] * k, -profile[i][1] * k)
-    s.mainloop()
+    l, r = -dx, dx
 
+    l_profile = list(filter(lambda xy: xy[0] < 0 and xy[1] < 0, ext_profile))
+    r_profile = list(filter(lambda xy: xy[0] >= 0 and xy[1] < 0, ext_profile))
 
-directory = "profiles"
-file_name = "file_1.csv"
+    while r - l > .1:
+        xl, xr = l + (r - l) / 3, l + (r - l) / 3 * 2
+        x0 = xl - dx
+        bf = bin_finder(list(map(lambda xy: xy[0], l_profile)), x0)
+        x1, x2 = l_profile[bf][0], l_profile[bf + 1][0]
+        y1, y2 = l_profile[bf][1], l_profile[bf + 1][1]
+        k = (y2 - y1) / (x2 - x1)
+        y0 = k * x0 + y1 - k * x1
 
-f = open(f"{directory}/{file_name}")
-c = list(csv.reader(f))
-c = list(map(lambda x: [float(x[0]), float(x[1])], c))
+        x0 = xl + dx
+        bf = bin_finder(list(map(lambda xy: xy[0], r_profile)), x0)
+        x1, x2 = r_profile[bf][0], r_profile[bf + 1][0]
+        y1, y2 = r_profile[bf][1], r_profile[bf + 1][1]
+        k = (y2 - y1) / (x2 - x1)
+        dyl = abs(y0 - (k * x0 + y1 - k * x1))
 
-stat_tube_pos(c, 0, 20)
+        x0 = xr - dx
+        bf = bin_finder(list(map(lambda xy: xy[0], l_profile)), x0)
+        x1, x2 = l_profile[bf][0], l_profile[bf + 1][0]
+        y1, y2 = l_profile[bf][1], l_profile[bf + 1][1]
+        k = (y2 - y1) / (x2 - x1)
+        y0 = k * x0 + y1 - k * x1
+
+        x0 = xr + dx
+        bf = bin_finder(list(map(lambda xy: xy[0], r_profile)), x0)
+        x1, x2 = r_profile[bf][0], r_profile[bf + 1][0]
+        y1, y2 = r_profile[bf][1], r_profile[bf + 1][1]
+        k = (y2 - y1) / (x2 - x1)
+        dyr = abs(y0 - (k * x0 + y1 - k * x1))
+
+        if dyl > dyr:
+            l = xl
+        else:
+            r = xr
+
+    center = [(l + r) / 2, 0.]
+    x0 = center[0] - dx
+
+    bf = bin_finder(list(map(lambda xy: xy[0], l_profile)), x0)
+    x1, x2 = l_profile[bf][0], l_profile[bf + 1][0]
+    y1, y2 = l_profile[bf][1], l_profile[bf + 1][1]
+    k = (y2 - y1) / (x2 - x1)
+    center[1] = k * x0 + y1 - k * x1
+
+    return [-center[0], -center[1]]
